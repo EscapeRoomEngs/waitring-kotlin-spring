20ad7a37ce413505eb44f0e62a1a94dcfcc20713
soohyun <soohyun6879@naver.com>
2023-12-07 22:20:28 +0900

    Feat: 쇼핑센터카테고리, 쇼핑센터 엔티티 및 테스트데이터 추가

---
+++ src/main/kotlin/com/waitring/waitring/mall/domain/MallCategory.kt
@@ -0,0 +1,37 @@
+package com.waitring.waitring.mall.domain
+
+import com.waitring.waitring.common.domain.Base
+import javax.persistence.*
+
+/** 쇼핑센터카테고리 */
+@Entity
+class MallCategory(
+        mallCtgNm : String,
+        seq : Int,
+        useFg : Boolean
+) : Base() {
+
+    /** 쇼핑센터카테고리 시퀀스번호 */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var mallCtgId : Long? = null
+
+    /** 쇼핑센터카테고리명 */
+    @Column(nullable = false, length = 80)
+    var mallCtgNm : String = mallCtgNm
+        protected set
+
+    /** 순번 */
+    @Column(nullable = false)
+    var seq : Int = seq
+        protected set
+
+    /**
+     * 사용여부
+     * true : 사용
+     * false : 미사용
+     */
+    @Column(nullable = false)
+    var useFg : Boolean = useFg
+        protected set
+}
---
+++ src/main/kotlin/com/waitring/waitring/mall/domain/MallState.kt
@@ -0,0 +1,7 @@
+package com.waitring.waitring.mall.domain
+
+enum class MallState(val value : String) {
+    OPERATE("운영"),
+    CLOSURE("폐점"),
+    STOP("휴점")
+}
